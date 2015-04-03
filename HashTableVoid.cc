fba293257e3071da8bee526841fa1f99d9315275
Tarang B Khanna <khanna17@borg18.cs.purdue.edu>
2015-04-03 16:04:26 -0400

    Make IRCServer

--- HashTableVoid.cc
+++ HashTableVoid.cc
@@ -110,7 +110,7 @@ bool HashTableVoidIterator::next(const char * & key, void * & data)
   if(_currentEntry != NULL) { 
        //_currentEntry = _hashTable->_buckets[_currentBucket];
        printf("_currentBucket: %d at %s HERE\n",(int*)_currentEntry->_data,_currentEntry->_key); // pass &key and data
-       //_currentEntry = _currentEntry->_next; //next linked list-- if not null return this val next time
+       _currentEntry = _currentEntry->_next; //next linked list-- if not null return this val next time
        if(_currentEntry != NULL) {
         _currentBucket++;
          return true;
@@ -122,7 +122,7 @@ bool HashTableVoidIterator::next(const char * & key, void * & data)
         //_currentEntry = _currentEntry->_next; 
        if(_currentEntry != NULL) {
          _currentBucket++;
-         printf("_currentBucket: %d at %s\n",(int*)_currentEntry->_data,_currentEntry->_key);
+         printf("_currentBucket: %d at %s\n",(int*)_currentEntry->_data , _currentEntry->_key);
          return true;
        }
     }
