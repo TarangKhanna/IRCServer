3f37516a3d47fe9c9e233c2167141c16a324e35a
Tarang B Khanna <khanna17@data.cs.purdue.edu>
2015-04-15 14:18:41 -0400

    Make IRCServer

--- IRCServer.cc
+++ IRCServer.cc
@@ -424,7 +424,7 @@ IRCServer::sendMessage(int fd, const char * user, const char * password, const c
        }
        roomCount++;
     }
-    const char * msg20 = pRoom;
+    const char * msg20 = args;
     write(fd, msg20, strlen(msg20));
     const char * msg21 = "THAT^\n";
     write(fd, msg21, strlen(msg21));
