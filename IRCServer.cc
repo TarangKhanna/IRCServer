baa9e155ec8b8aff48894c51f63d9b34dd46b239
Tarang B Khanna <khanna17@borg03.cs.purdue.edu>
2015-04-17 17:48:29 -0400

    Make IRCServer

--- IRCServer.cc
+++ IRCServer.cc
@@ -450,10 +450,20 @@ IRCServer::getMessages(int fd, const char * user, const char * password, const c
        }
        roomCount++;
     }
-    HashTableVoidIterator iterator2(&h2); // messages and user table
-    const char * msg;
+    const char * key5;
     void * gradev;
-    iterator2.next3(fd,msg, gradev, roomCount, from);
+    HashTableVoidIterator iterator(&h);
+    if(!iterator.userInRoomExists(fd,key5, gradev, user,roomCount)) {
+        const char * msg3 =  "ERROR (No user in room)\r\n";
+        write(fd, msg3, strlen(msg3));
+    } else {
+        const char * msg2 =  "OK\r\n";
+        write(fd, msg2, strlen(msg2)); // messages and user table
+        const char * msg;
+        void * gradev;
+        HashTableVoidIterator iterator2(&h2); 
+        iterator2.next3(fd,msg, gradev, roomCount, from);
+      }
   } else {
       const char * msg =  "ERROR (Wrong password)\r\n";
       write(fd, msg, strlen(msg));
